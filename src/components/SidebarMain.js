82099da1aa77a32c991eea4c0472e4e148b3f154
Alejandro Martir <hola@alemartir.com>
2024-08-07 00:00:04 +0000

    Fixed margin bottom

--- src/components/SidebarMain.js
+++ src/components/SidebarMain.js
@@ -3,7 +3,9 @@ import SidebarLatestArticles from "../components/SidebarLatestArticles";
 export default function SidebarMain() {
   return (
     <div>
-      <SidebarLatestArticles />
+      <div className="-mb-4">
+        <SidebarLatestArticles />
+      </div>
     </div>
   );
 }
