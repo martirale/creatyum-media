be8097e1c860ee5b86a72642338086ac4043beb4
Alejandro Martir <hola@alemartir.com>
2024-08-13 04:47:13 +0000

    Create featured section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import HomePageContent from "../components/HomePageContent";
+import HomePageFeatured from "../components/HomePageFeatured";
 
 export default function HomePage() {
   return (
     <div className="container mx-auto px-4 py-2 md:px-0">
       <h1>Creatyum Media</h1>
+
+      <HomePageFeatured />
 
       <HomePageContent />
     </div>
