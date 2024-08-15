7066d715df66c48c7c845ae8b653b76ae5c67cd0
Alejandro Martir <hola@alemartir.com>
2024-08-15 07:55:12 +0000

    Update default meta-tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,29 @@ export const metadata = {
   title: "Creatyum Media — Divulgación sobre diseño y creatividad",
   description:
     "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+  openGraph: {
+    title: "Creatyum Media — Divulgación sobre diseño y creatividad",
+    description:
+      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+    url: "https://creatyum.media",
+    type: "website",
+    images: [
+      {
+        url: "https://creatyum.media/creatyum-default-cover.webp",
+        width: 1200,
+        height: 630,
+        alt: "Creatyum Media",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Creatyum Media — Divulgación sobre diseño y creatividad",
+    description:
+      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+    images: ["https://creatyum.media/creatyum-default-cover.webp"],
+  },
+  canonical: "https://creatyum.media",
 };
 
 export default function RootLayout({ children }) {
