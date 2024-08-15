26b8268263b39bb5a44bec7636dd16f24971a087
Alejandro Martir <hola@alemartir.com>
2024-08-15 03:41:23 +0000

    Add featured recent post section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { usePathname } from "next/navigation";
+import FooterFeaturedArticles from "../components/FooterFeaturedArticles";
 
 export default function Footer() {
   const [theme, setTheme] = useState("light");
@@ -45,69 +47,88 @@ export default function Footer() {
     }
   };
 
+  const pathname = usePathname();
+  const isArticlePage = pathname.startsWith("/articulo/");
+
   return (
-    <footer className="border-t-black border-t-[1px] text-black dark:border-t-yellow dark:border-t-[1px] dark:text-yellow p-6 mt-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="flex items-center mt-8 mb-16 md:mt-0 md:mb-0">
-          <Link href="/">
-            <div className="logo-icon"></div>
-          </Link>
-        </div>
-        <div className="flex flex-col text-center font-BricolageGrotesque font-extrabold md:flex-row md:items-center md:font-Roboto md:font-normal space-y-5 md:space-y-0 md:space-x-8 mb-4 md:mb-0">
-          <Link href="/about" className="text-5xl hover:underline md:text-base">
-            About
-          </Link>
-          <Link
-            href="/categorias"
-            className="text-5xl hover:underline md:hidden"
-          >
-            Categorías
-          </Link>
-          <Link href="/podcast" className="text-5xl hover:underline md:hidden">
-            Podcast
-          </Link>
-          <div className="pt-12 md:pt-0">
-            <Link
-              href="/privacidad"
-              className="text-2xl mr-4 hover:underline md:text-base md:mr-8"
-            >
-              Privacidad
-            </Link>
-            <Link
-              href="/terminos"
-              className="text-2xl mr-4 hover:underline md:text-base md:mr-8"
-            >
-              Términos
-            </Link>
-            <button
-              onClick={toggleTheme}
-              className="text-2xl focus:outline-none md:text-base"
-            >
-              <span className="mr-2 hover:underline">Modo</span>
-              {theme === "dark" ? (
-                <FontAwesomeIcon
-                  icon={faSun}
-                  className="text-yellow w-6 h-6 md:w-4 md:h-4"
-                />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faMoon}
-                  className="text-black w-6 h-6 md:w-4 md:h-4"
-                />
-              )}
-            </button>
+    <footer className="mt-8">
+      {isArticlePage && (
+        <div className="bg-black dark:bg-yellow">
+          <div className="container mx-auto px-8 py-4 justify-between items-center md:px-0 md:py-8">
+            <FooterFeaturedArticles />
           </div>
         </div>
-        <div className="text-xs p-12 text-center md:flex md:items-center md:p-0">
-          <p>
-            <a
-              href="https://alemartir.com"
-              target="_blank"
-              className="hover:underline"
+      )}
+
+      <div className="border-t-black border-t-[1px] text-black dark:border-t-yellow dark:border-t-[1px] dark:text-yellow">
+        <div className="container mx-auto px-4 py-6 flex flex-col md:flex-row justify-between items-center">
+          <div className="flex items-center mt-8 mb-16 md:mt-0 md:mb-0">
+            <Link href="/">
+              <div className="logo-icon"></div>
+            </Link>
+          </div>
+          <div className="flex flex-col text-center font-BricolageGrotesque font-extrabold md:flex-row md:items-center md:font-Roboto md:font-normal space-y-5 md:space-y-0 md:space-x-8 mb-4 md:mb-0">
+            <Link
+              href="/about"
+              className="text-5xl hover:underline md:text-base"
             >
-              &copy; COPYRIGHT 2024 AM
-            </a>
-          </p>
+              About
+            </Link>
+            <Link
+              href="/categorias"
+              className="text-5xl hover:underline md:hidden"
+            >
+              Categorías
+            </Link>
+            <Link
+              href="/podcast"
+              className="text-5xl hover:underline md:hidden"
+            >
+              Podcast
+            </Link>
+            <div className="pt-12 md:pt-0">
+              <Link
+                href="/privacidad"
+                className="text-2xl mr-4 hover:underline md:text-base md:mr-8"
+              >
+                Privacidad
+              </Link>
+              <Link
+                href="/terminos"
+                className="text-2xl mr-4 hover:underline md:text-base md:mr-8"
+              >
+                Términos
+              </Link>
+              <button
+                onClick={toggleTheme}
+                className="text-2xl focus:outline-none md:text-base"
+              >
+                <span className="mr-2 hover:underline">Modo</span>
+                {theme === "dark" ? (
+                  <FontAwesomeIcon
+                    icon={faSun}
+                    className="text-yellow w-6 h-6 md:w-4 md:h-4"
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    icon={faMoon}
+                    className="text-black w-6 h-6 md:w-4 md:h-4"
+                  />
+                )}
+              </button>
+            </div>
+          </div>
+          <div className="text-xs p-12 text-center md:flex md:items-center md:p-0">
+            <p>
+              <a
+                href="https://alemartir.com"
+                target="_blank"
+                className="hover:underline"
+              >
+                &copy; COPYRIGHT 2024 AM
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </footer>
---
+++ src/components/FooterFeaturedArticles.js
@@ -0,0 +1,152 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarDays,
+  faFire,
+  faTag,
+} from "@fortawesome/free-solid-svg-icons";
+import { getArticles } from "../lib/api";
+
+function FeaturedArticleList({ article, isMain }) {
+  if (isMain) {
+    return (
+      <div
+        className="col-span-12 row-span-12 relative bg-cover bg-center rounded-3xl border border-yellow text-yellow hover:text-black aspect-w-1 aspect-h-1 md:col-span-2 md:row-span-2 md:aspect-w-5 md:aspect-h-4 dark:hover:text-yellow dark:border-black"
+        style={{
+          backgroundImage: `url(${process.env.NEXT_PUBLIC_STRAPI_API_URL}${article.attributes.cover.data.attributes.url})`,
+        }}
+      >
+        <Link href={`/articulo/${article.attributes.slug}`}>
+          <div className="absolute inset-0 bg-black bg-opacity-10 rounded-3xl flex items-center justify-between p-4 text-center hover:bg-yellow hover:bg-opacity-100 md:p-24 dark:hover:bg-black duration-300">
+            <div>
+              <p>
+                <FontAwesomeIcon
+                  icon={faCalendarDays}
+                  className="mr-1 w-4 h-4"
+                />
+                {new Intl.DateTimeFormat("es-ES", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                }).format(new Date(article.attributes.date))}
+
+                <FontAwesomeIcon icon={faTag} className="ml-4 mr-1 w-4 h-4" />
+                {article.attributes.categories.data.map((category, index) => (
+                  <span key={index}>{category.attributes.title}</span>
+                ))}
+              </p>
+              <h2 className="text-5xl font-extrabold mt-2 md:text-7xl">
+                {article.attributes.title}
+              </h2>
+            </div>
+          </div>
+        </Link>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Link href={`/articulo/${article.attributes.slug}`}>
+          <div className="flex items-center space-x-4 mb-4">
+            {article.attributes.cover && article.attributes.cover.data && (
+              <img
+                src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${article.attributes.cover.data.attributes.url}`}
+                alt={article.attributes.title}
+                className="w-16 h-16 object-cover rounded-full border border-yellow md:w-20 md:h-20 dark:border-black"
+              />
+            )}
+            <div className="flex flex-col justify-center">
+              <h3 className="text-2xl font-extrabold md:text-3xl hover:underline">
+                {article.attributes.title}
+              </h3>
+              <p className="text-sm mt-2">
+                {new Intl.DateTimeFormat("es-ES", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                }).format(new Date(article.attributes.date))}
+              </p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default function FeaturedArticles() {
+  const [featuredArticles, setFeaturedArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchFeaturedArticles();
+  }, []);
+
+  const fetchFeaturedArticles = async () => {
+    setIsLoading(true);
+    try {
+      const data = await getArticles(1, 10, {
+        sort: ["date:desc"],
+      });
+
+      const filteredArticles = data.data.filter(
+        (article) => article.attributes.featured === true
+      );
+
+      const limitedArticles = filteredArticles.slice(0, 7);
+
+      setFeaturedArticles(limitedArticles);
+    } catch (error) {
+      console.error("Error fetching featured articles:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="text-yellow dark:text-black">
+      <div className="mb-5 md:mb-7">
+        <h2 className="font-extrabold text-lg uppercase md:text-2xl">
+          <FontAwesomeIcon
+            icon={faFire}
+            className="w-3.5 h-3.5 align-baseline md:w-4 md:h-4"
+          />{" "}
+          Destacados
+        </h2>
+      </div>
+
+      <div className="grid grid-cols-3 gap-6">
+        {isLoading ? (
+          <p className="text-center col-span-3 text-white">
+            Cargando artículos destacados...
+          </p>
+        ) : (
+          <>
+            {featuredArticles.length > 0 && (
+              <>
+                {/* Artículo más reciente */}
+                <FeaturedArticleList
+                  article={featuredArticles[0]}
+                  isMain={true}
+                />
+                <div className="col-span-12 flex flex-col gap-4 md:col-span-1">
+                  {/* Siguientes 4 artículos destacados */}
+                  {featuredArticles.slice(1, 7).map((article) => (
+                    <FeaturedArticleList
+                      key={article.id}
+                      article={article}
+                      isMain={false}
+                    />
+                  ))}
+                </div>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
