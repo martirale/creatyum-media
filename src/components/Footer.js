cbf91b0a115d2a9757df525525adbb53a800c871
Alejandro Martir <hola@alemartir.com>
2024-08-17 04:01:12 +0000

    Change dark mode switch location

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,9 @@
 "use client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { usePathname } from "next/navigation";
 import FooterFeaturedArticles from "../components/FooterFeaturedArticles";
 
 export default function Footer() {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    const currentTheme = localStorage.getItem("theme");
-    if (currentTheme === "dark") {
-      document.documentElement.classList.add("dark");
-      setTheme("dark");
-      setCSSVariables("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      setTheme("light");
-      setCSSVariables("light");
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    if (theme === "dark") {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      setTheme("light");
-      setCSSVariables("light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setTheme("dark");
-      setCSSVariables("dark");
-    }
-  };
-
-  const setCSSVariables = (theme) => {
-    const root = document.documentElement;
-    if (theme === "dark") {
-      root.style.setProperty("--logo", "url('/creatyum-logo-y.svg')");
-      root.style.setProperty("--logo-icon", "url('/creatyum-icon-y.svg')");
-    } else {
-      root.style.setProperty("--logo", "url('/creatyum-logo-k.svg')");
-      root.style.setProperty("--logo-icon", "url('/creatyum-icon-k.svg')");
-    }
-  };
-
   const pathname = usePathname();
   const isArticlePage = pathname.startsWith("/articulo/");
   const isCategoriesPage = pathname.startsWith("/categorias");
@@ -113,27 +70,10 @@ export default function Footer() {
               </Link>
               <Link
                 href="/terminos"
-                className="text-2xl mr-4 hover:underline md:text-base md:mr-8"
+                className="text-2xl hover:underline md:text-base"
               >
                 Términos
               </Link>
-              <button
-                onClick={toggleTheme}
-                className="text-2xl focus:outline-none md:text-base"
-              >
-                <span className="mr-2 hover:underline">Modo</span>
-                {theme === "dark" ? (
-                  <FontAwesomeIcon
-                    icon={faSun}
-                    className="text-yellow w-6 h-6 md:w-4 md:h-4"
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    icon={faMoon}
-                    className="text-black w-6 h-6 md:w-4 md:h-4"
-                  />
-                )}
-              </button>
             </div>
           </div>
           <div className="text-xs p-12 text-center md:flex md:items-center md:p-0">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,55 @@
 "use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBarsStaggered, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSun,
+  faMoon,
+  faBarsStaggered,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+      setCSSVariables("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      setTheme("light");
+      setCSSVariables("light");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setTheme("light");
+      setCSSVariables("light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setTheme("dark");
+      setCSSVariables("dark");
+    }
+  };
+
+  const setCSSVariables = (theme) => {
+    const root = document.documentElement;
+    if (theme === "dark") {
+      root.style.setProperty("--logo", "url('/creatyum-logo-y.svg')");
+      root.style.setProperty("--logo-icon", "url('/creatyum-icon-y.svg')");
+    } else {
+      root.style.setProperty("--logo", "url('/creatyum-logo-k.svg')");
+      root.style.setProperty("--logo-icon", "url('/creatyum-icon-k.svg')");
+    }
+  };
+
   const [isOpen, setIsOpen] = useState(false);
 
   function getMenuClasses() {
@@ -58,13 +103,23 @@ export default function Navbar() {
           >
             Podcast
           </Link>
-          {/* <Link
-            href="https://archive.creatyum.com"
-            target="_blank"
-            className="font-BricolageGrotesque text-5xl mx-2 hover:underline md:pl-4"
+          <button
+            onClick={toggleTheme}
+            className="font-BricolageGrotesque text-5xl text-left mx-2 md:pl-4"
           >
-            Archivo
-          </Link> */}
+            <span className="mr-2 md:hidden">Modo</span>
+            {theme === "dark" ? (
+              <FontAwesomeIcon
+                icon={faSun}
+                className="text-black align-baseline w-8 h-8 md:text-yellow"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faMoon}
+                className="text-yellow align-baseline w-8 h-8 md:text-black"
+              />
+            )}
+          </button>
         </div>
 
         <div className="flex items-center md:hidden">
