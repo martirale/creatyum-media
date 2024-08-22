7c891047803d97f212c8b66298079c5dbc308076
Alejandro Martir <hola@alemartir.com>
2024-08-22 07:11:58 +0000

    Restore GA4 component

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ScrollToTop from "../components/ScrollToTop";
 import BackToTop from "../components/BackToTop";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 import CookieConsentManager from "../components/CookieConsentManager";
 
 export const metadata = {
