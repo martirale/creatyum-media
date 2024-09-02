f3e69632457154d444af227e9b390c18704708b0
Alejandro Mártir <hola@alemartir.com>
2024-09-02 03:26:11 -0600

    Add dark mode classes

--- src/components/testimonial/TestimonialCard.js
+++ src/components/testimonial/TestimonialCard.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function TestimonialCard({ testimonial }) {
   return (
-    <div className="bg-black rounded-2xl p-6 mx-2 my-4 text-yellow text-center md:rounded-3xl">
+    <div className="bg-black rounded-2xl p-6 mx-0 my-4 text-yellow text-center md:rounded-3xl dark:bg-yellow dark:text-black">
       <div className="w-24 h-24 mx-auto mb-4 relative">
         <Image
           src={testimonial.profileImage}
           alt={testimonial.name}
           fill
-          className="rounded-full object-cover"
+          className="rounded-full object-cover border border-yellow dark:border-black"
         />
       </div>
       <p className="text-lg mb-4">&ldquo;{testimonial.quote}&rdquo;</p>
