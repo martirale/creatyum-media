c2a8a3fb2eaf0d97ed9e94aeccde6b84836665ab
Alejandro Martir <hola@alemartir.com>
2024-09-03 18:36:45 -0600

    Change name and episode font size

--- src/components/testimonial/TestimonialCard.js
+++ src/components/testimonial/TestimonialCard.js
@@ -7,7 +7,7 @@ export default function TestimonialCard({ testimonial }) {
 
   return (
     <div className="bg-black rounded-2xl px-8 py-12 mx-0 my-4 text-yellow text-center md:rounded-3xl dark:bg-yellow dark:text-black flex flex-col h-full">
-      <div className="w-24 h-24 mx-auto mb-4 relative flex-shrink-0">
+      <div className="w-24 h-24 mx-auto mb-8 relative flex-shrink-0">
         <Image
           src={imageUrl}
           alt={testimonial.name}
@@ -18,7 +18,7 @@ export default function TestimonialCard({ testimonial }) {
       <div className="flex flex-col justify-between flex-grow">
         <p className="text-lg mb-4">&ldquo;{testimonial.quote}&rdquo;</p>
         <div className="mt-auto">
-          <h4 className="uppercase font-extrabold">
+          <h4 className="uppercase font-extrabold text-lg">
             {testimonial.name}
             <br />
             {testimonial.episode}
