060fa7896f9f6d9ccd63300d78e5e58490524433
Alejandro Martir <hola@alemartir.com>
2024-09-03 18:26:49 -0600

    Fix card height

--- src/components/testimonial/TestimonialCard.js
+++ src/components/testimonial/TestimonialCard.js
@@ -6,8 +6,8 @@ export default function TestimonialCard({ testimonial }) {
     : null;
 
   return (
-    <div className="bg-black rounded-2xl px-8 py-12 mx-0 my-4 text-yellow text-center md:rounded-3xl dark:bg-yellow dark:text-black">
-      <div className="w-24 h-24 mx-auto mb-4 relative">
+    <div className="bg-black rounded-2xl px-8 py-12 mx-0 my-4 text-yellow text-center md:rounded-3xl dark:bg-yellow dark:text-black flex flex-col h-full">
+      <div className="w-24 h-24 mx-auto mb-4 relative flex-shrink-0">
         <Image
           src={imageUrl}
           alt={testimonial.name}
@@ -15,13 +15,15 @@ export default function TestimonialCard({ testimonial }) {
           className="rounded-full object-cover border border-yellow dark:border-black"
         />
       </div>
-      <p className="text-lg mb-4">&ldquo;{testimonial.quote}&rdquo;</p>
-      <div className="flex flex-col items-center">
-        <h4 className="uppercase font-extrabold">
-          {testimonial.name}
-          <br />
-          {testimonial.episode}
-        </h4>
+      <div className="flex flex-col justify-between flex-grow">
+        <p className="text-lg mb-4">&ldquo;{testimonial.quote}&rdquo;</p>
+        <div className="mt-auto">
+          <h4 className="uppercase font-extrabold">
+            {testimonial.name}
+            <br />
+            {testimonial.episode}
+          </h4>
+        </div>
       </div>
     </div>
   );
