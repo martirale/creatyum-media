3d046dc8491d606207fd8a174c6989106fb36adf
Alejandro Martir <hola@alemartir.com>
2024-09-16 00:41:46 -0600

    Add RSS route

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,7 @@ export const metadata = {
     images: ["https://creatyum.media/creatyum-default-cover.webp"],
   },
   canonical: "https://creatyum.media",
+  rss: "https://creatyum.media/api/rss",
   icons: {
     icon: "/favicon.png",
   },
@@ -47,6 +48,12 @@ export default function RootLayout({ children }) {
         {process.env.NODE_ENV === "production" && (
           <GoogleAnalytics gaId="G-V5TV2RSE21" />
         )}
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="Creatyum Media"
+          href="https://creatyum.media/api/rss"
+        />
       </head>
       <body
         className={`${Roboto.variable} ${BricolageGrotesque.variable} font-Roboto font-light bg-yellow text-black dark:bg-black dark:text-yellow custom-vh`}
