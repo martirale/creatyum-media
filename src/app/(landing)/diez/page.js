331ff66c2f654e355379d49dd159a0b6089b3bc9
Alejandro Martir <hola@alemartir.com>
2024-09-18 01:34:37 -0600

    Create aniversary page

---
+++ src/app/(landing)/diez/page.js
@@ -0,0 +1,40 @@
+export const metadata = {
+  title: "Décimo aniversario — Creatyum Media",
+  description:
+    "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+  openGraph: {
+    title: "Décimo aniversario — Creatyum Media",
+    description:
+      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+    url: "https://creatyum.media/diez",
+    type: "website",
+    images: [
+      {
+        url: "https://creatyum.media/creatyum-default-cover.webp",
+        width: 1200,
+        height: 630,
+        alt: "Creatyum Media",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Décimo aniversario — Creatyum Media",
+    description:
+      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+    images: ["https://creatyum.media/creatyum-default-cover.webp"],
+  },
+  canonical: "https://creatyum.media/diez",
+};
+
+export default function AniversaryLanding() {
+  return (
+    <div className="container mx-auto px-4 py-2 md:px-0">
+      <h1>Décimo aniversario</h1>
+
+      <div className="grid grid-cols-12 gap-4 md:gap-12">
+        <div className="col-span-12 md:col-span-8"></div>
+      </div>
+    </div>
+  );
+}
