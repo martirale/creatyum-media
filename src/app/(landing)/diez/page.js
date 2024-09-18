a21362b87368f8587f78cc208ed5f9d0ba8e243e
Alejandro Martir <hola@alemartir.com>
2024-09-18 17:19:21 -0600

    Add IG and comic banners

--- src/app/(landing)/diez/page.js
+++ src/app/(landing)/diez/page.js
@@ -1,3 +1,6 @@
+import BannerComic from "../../../components/home/BannerComic";
+import BannerInstagram from "../../../components/home/BannerInstagram";
+
 export const metadata = {
   title: "Décimo aniversario — Creatyum Media",
   description:
@@ -32,9 +35,9 @@ export default function AniversaryLanding() {
     <div className="container mx-auto px-4 py-2 md:px-0">
       <h1>Décimo aniversario</h1>
 
-      <div className="grid grid-cols-12 gap-4 md:gap-12">
-        <div className="col-span-12 md:col-span-8"></div>
-      </div>
+      <BannerInstagram />
+
+      <BannerComic />
     </div>
   );
 }
