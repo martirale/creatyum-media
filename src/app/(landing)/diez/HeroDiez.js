30b830645d87a3ef79b8013077f9d33791168457
Alejandro Martir <hola@alemartir.com>
2024-09-19 21:45:16 -0600

    Add foundation date

--- src/app/(landing)/diez/HeroDiez.js
+++ src/app/(landing)/diez/HeroDiez.js
@@ -7,14 +7,16 @@ const HeroDiez = () => {
       <div className="rounded-2xl bg-black text-yellow dark:bg-yellow dark:text-black md:p-8 md:rounded-3xl">
         <div className="flex flex-wrap items-center w-full">
           <div className="w-full md:w-1/2 p-4 md:p-0">
-            <CafeCreativoIllustration className="fill-yellow w-full h-auto mt-8 dark:fill-black" />
+            <CafeCreativoIllustration className="fill-yellow w-full h-auto my-8 dark:fill-black" />
           </div>
 
           <div className="w-full md:w-1/2 p-4 flex flex-col items-center">
-            <div className="text-center mt-8 md:px-2">
+            <div className="text-center md:px-2">
               <h2 className="font-BricolageGrotesque font-extrabold text-4xl md:text-7xl">
                 ¡Diez años divulgando sobre diseño!
               </h2>
+
+              <p className="text-2xl md:text-3xl mt-5 md:mt-8">Octubre 2024</p>
             </div>
           </div>
 
