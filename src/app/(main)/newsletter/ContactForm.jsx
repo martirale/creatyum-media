08065a086a6373ce1273ee09147b518cb3644a40
Alejandro Martir <hola@alemartir.com>
2024-10-17 15:37:22 -0600

    Create contact form init files from Creatyum Studio's web

---
+++ src/app/(main)/newsletter/ContactForm.jsx
@@ -0,0 +1,120 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { handleForm } from "../../../utils/ContactAction";
+import DOMPurify from "dompurify";
+
+export default function ContactForm() {
+  const [successMessage, setSuccessMessage] = useState("");
+  const [acceptPrivacyPolicy, setAcceptPrivacyPolicy] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const company = formData.get("company");
+    const content = formData.get("content");
+    const sanitizedContent = DOMPurify.sanitize(content);
+    const data = {
+      name,
+      email,
+      company,
+      content: sanitizedContent,
+    };
+
+    if (!acceptPrivacyPolicy) {
+      setSuccessMessage("Debes aceptar la política de privacidad.");
+      setTimeout(() => {
+        setSuccessMessage("");
+      }, 3000);
+      return;
+    }
+
+    const result = await handleForm(data);
+
+    if (result) {
+      setSuccessMessage("¡Correo enviado!");
+
+      event.target.reset();
+      setAcceptPrivacyPolicy(false);
+
+      setTimeout(() => {
+        setSuccessMessage("");
+      }, 3000);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      {successMessage && (
+        <div className="mb-4 p-3 w-full rounded-full font-bold text-center text-[#2F855A] bg-[#B2F5EA]">
+          {successMessage}
+        </div>
+      )}
+      <form onSubmit={handleSubmit} className="flex flex-col w-full gap-4">
+        <input
+          type="text"
+          placeholder="Nombre y apellido"
+          name="name"
+          maxLength="50"
+          required
+          className="px-5 py-3 rounded-full block bg-yellow text-black border border-black dark:bg-black dark:text-yellow dark:border-yellow"
+        />
+        <input
+          type="email"
+          placeholder="Correo electrónico"
+          name="email"
+          maxLength="100"
+          required
+          className="px-5 py-3 rounded-full block bg-yellow text-black border border-black dark:bg-black dark:text-yellow dark:border-yellow"
+        />
+        <input
+          type="text"
+          placeholder="Empresa (opcional)"
+          name="company"
+          maxLength="50"
+          className="px-5 py-3 rounded-full block bg-yellow text-black border border-black dark:bg-black dark:text-yellow dark:border-yellow"
+        />
+        <textarea
+          placeholder="¿En qué podemos ayudarte?"
+          name="content"
+          maxLength="500"
+          required
+          className="px-5 py-3 rounded-3xl h-32 block bg-yellow text-black border border-black dark:bg-black dark:text-yellow dark:border-yellow"
+        />
+
+        <div className="flex items-center mb-4">
+          <input
+            type="checkbox"
+            id="privacyPolicy"
+            name="privacyPolicy"
+            checked={acceptPrivacyPolicy}
+            onChange={() => setAcceptPrivacyPolicy(!acceptPrivacyPolicy)}
+            className="mr-2"
+            required
+          />
+          <label htmlFor="privacyPolicy" className="text-sm">
+            Acepto la{" "}
+            <Link
+              href="/privacidad"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              política de privacidad
+            </Link>
+            .
+          </label>
+        </div>
+
+        <button className="px-5 py-3 rounded-full font-BricolageGrotesque font-bold text-2xl uppercase bg-black text-yellow border border-black hover:bg-yellow hover:text-black dark:bg-yellow dark:text-black dark:border-yellow dark:hover:bg-black dark:hover:text-yellow">
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ src/app/(main)/newsletter/page.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import ContactForm from "./ContactForm";
+
+export const metadata = {
+  title: "Newsletter | Creatyum Media",
+  description:
+    "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+  openGraph: {
+    title: "Newsletter | Creatyum Media",
+    description:
+      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+    url: "https://creatyum.media/newsletter",
+    type: "website",
+    images: [
+      {
+        url: "https://creatyum.media/creatyum-default-cover.webp",
+        width: 1200,
+        height: 630,
+        alt: "Creatyum Media",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Newsletter | Creatyum Media",
+    description:
+      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+    images: ["https://creatyum.media/creatyum-default-cover.webp"],
+  },
+  canonical: "https://creatyum.media/newsletter",
+};
+
+export default function ContactPage() {
+  return (
+    <div className="container mx-auto px-4 py-2 md:px-0">
+      <h1>Newsletter</h1>
+
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-12">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-6xl md:text-7xl font-extrabold mb-8">
+            Newsletter
+          </h2>
+          <p className="mb-16 md:mb-0">Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <ContactForm />
+        </div>
+      </div>
+    </div>
+  );
+}
