684b97ccdc971dea5a6f4644336ba0dbc602b7c4
Alejandro Martir <hola@alemartir.com>
2024-10-17 03:22:38 -0600

    Fix rss API

--- src/app/api/rss/route.js
+++ src/app/api/rss/route.js
@@ -17,18 +17,27 @@ export async function GET() {
 
     // Agregar los artículos al feed RSS
     articles.forEach((article) => {
-      const coverImage = article.attributes.cover?.data?.attributes?.url;
+      const coverImage = article.cover?.url;
       const coverImageUrl = coverImage
         ? `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${coverImage}`
         : null;
 
+      // Detectar el tipo de imagen por la extensión del archivo
+      const imageType = coverImageUrl
+        ? coverImageUrl.endsWith(".webp")
+          ? "image/webp"
+          : coverImageUrl.endsWith(".gif")
+            ? "image/gif"
+            : "image/webp"
+        : undefined;
+
       feed.item({
-        title: article.attributes.title,
-        description: article.attributes.description,
-        url: `https://creatyum.media/articulo/${article.attributes.slug}`,
-        date: article.attributes.date,
+        title: article.title,
+        description: article.description,
+        url: `https://creatyum.media/articulo/${article.slug}`,
+        date: article.date,
         enclosure: coverImageUrl
-          ? { url: coverImageUrl, type: "image/jpeg" }
+          ? { url: coverImageUrl, type: imageType }
           : undefined,
       });
     });
