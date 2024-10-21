41293a3f6df0fd8d5afc6cc1fdc18674eab588ed
Alejandro Martir <hola@alemartir.com>
2024-10-20 20:29:44 -0600

    Add newsletter's illustration

--- src/components/hero/HeroNewsletter.jsx
+++ src/components/hero/HeroNewsletter.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import LogoCreatyum from "@logos/LogoCreatyum";
-import CafeCreativoIllustration from "@components/illustrations/CafeCreativoIllustration";
+import Image from "next/image";
 
 const HeroNewsletter = () => {
   return (
@@ -8,7 +7,13 @@ const HeroNewsletter = () => {
       <div className="rounded-2xl bg-black text-yellow dark:bg-yellow dark:text-black md:rounded-3xl">
         <div className="flex flex-wrap items-center w-full">
           <div className="w-full md:w-1/2">
-            <CafeCreativoIllustration className="fill-yellow w-full h-auto pt-8 px-10 2xl:pt-10 2xl:px-12 dark:fill-black" />
+            <Image
+              src="/NewsletterIllustration.svg"
+              alt="Newsletter illustration."
+              width="1080"
+              height="1050"
+              className="pt-8 px-8 border border-b-yellow md:border-b-0 dark:border-b-black"
+            />
           </div>
 
           <div className="w-full md:w-1/2 p-4 md:p-8 flex flex-col items-center">
@@ -17,8 +22,6 @@ const HeroNewsletter = () => {
                 ¡Únete a nuestra comunidad creativa!
               </h2>
             </div>
-
-            <LogoCreatyum className="fill-yellow w-32 h-auto mt-16 mb-4 md:mb-0 dark:fill-black" />
           </div>
         </div>
       </div>
