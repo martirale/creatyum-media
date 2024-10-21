f1b558d2ad981d57b9356edc552dc963ab9a17c5
Alejandro Martir <hola@alemartir.com>
2024-10-21 01:21:43 -0600

    Add content

--- src/components/newsletter/ValuesBadges.jsx
+++ src/components/newsletter/ValuesBadges.jsx
@@ -1,24 +1,37 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faNewspaper,
+  faArrowTrendUp,
+  faPodcast,
+  faToolbox,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function ValuesBadges({ className = "" }) {
   return (
     <section className={`w-full ${className}`}>
+      <h2 className="text-5xl text-center font-extrabold mb-8">
+        ¿Qué esperar?
+      </h2>
+
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <div className="text-center p-4 border border-black rounded-2xl dark:border-yellow">
-          <span className="block font-BricolageGrotesque text-5xl"></span>
-          <span className="block uppercase font-bold">Días</span>
+          <FontAwesomeIcon icon={faNewspaper} className="w-16 h-16 mb-4" />
+          <span className="block uppercase font-bold">Resúmenes semanales</span>
         </div>
         <div className="text-center p-4 border border-black rounded-2xl dark:border-yellow">
-          <span className="block font-BricolageGrotesque text-5xl"></span>
-          <span className="block uppercase font-bold">Horas</span>
+          <FontAwesomeIcon icon={faArrowTrendUp} className="w-16 h-16 mb-4" />
+          <span className="block uppercase font-bold">
+            Tendencias y novedades
+          </span>
         </div>
         <div className="text-center p-4 border border-black rounded-2xl dark:border-yellow">
-          <span className="block font-BricolageGrotesque text-5xl"></span>
-          <span className="block uppercase font-bold">Minutos</span>
+          <FontAwesomeIcon icon={faPodcast} className="w-16 h-16 mb-4" />
+          <span className="block uppercase font-bold">Nuevos podcasts</span>
         </div>
         <div className="text-center p-4 border border-black rounded-2xl dark:border-yellow">
-          <span className="block font-BricolageGrotesque text-5xl"></span>
-          <span className="block uppercase font-bold">Segundos</span>
+          <FontAwesomeIcon icon={faToolbox} className="w-16 h-16 mb-4" />
+          <span className="block uppercase font-bold">Tips y herramientas</span>
         </div>
       </div>
     </section>
