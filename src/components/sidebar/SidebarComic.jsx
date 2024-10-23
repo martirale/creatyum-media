7f642bbb03c0194069273c68d8c1df792042dbd4
Alejandro Martir <hola@alemartir.com>
2024-10-23 01:55:54 -0600

    Refactorize fetch content

--- src/components/sidebar/SidebarComic.jsx
+++ src/components/sidebar/SidebarComic.jsx
@@ -1,78 +1,58 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import LogoLayered from "@logos/LogoLayered";
 import { getLatestComicImage } from "@lib/api";
 import SidebarContainer from "@ui/sidebar/SidebarContainer";
 
-const SidebarComic = () => {
-  const [comicImageUrl, setComicImageUrl] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+export default async function SidebarComic() {
+  try {
+    const imageUrl = await getLatestComicImage();
 
-  useEffect(() => {
-    const fetchComic = async () => {
-      try {
-        const imageUrl = await getLatestComicImage();
-        if (imageUrl) {
-          setComicImageUrl(imageUrl);
-        } else {
-          setError("No comic image found");
-        }
-      } catch (err) {
-        console.error("Error fetching comic:", err);
-        setError("Failed to load comic image");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchComic();
-  }, []);
-
-  return (
-    <SidebarContainer>
-      <div className="container mx-auto">
-        <div className="flex flex-wrap items-center w-full">
-          <div className="w-full p-4 flex flex-col items-center">
-            <LogoLayered className="fill-yellow w-64 h-[122px] dark:fill-black" />
-            <div className="text-center mt-8">
-              <p className="mb-8">
-                <b>
-                  ¡Nueva viñeta cada semana!
-                  <br />
-                  <Link
-                    href="https://www.instagram.com/explore/tags/creatyumlayered/"
-                    rel="noreferrer noopener"
-                    target="_blank"
-                    className="underline"
-                  >
-                    #creatyumlayered
-                  </Link>
-                </b>
-              </p>
+    return (
+      <SidebarContainer>
+        <div className="container mx-auto">
+          <div className="flex flex-wrap items-center w-full">
+            <div className="w-full p-4 flex flex-col items-center">
+              <LogoLayered className="fill-yellow w-64 h-[122px] dark:fill-black" />
+              <div className="text-center mt-8">
+                <p className="mb-8">
+                  <b>
+                    ¡Nueva viñeta cada semana!
+                    <br />
+                    <Link
+                      href="https://www.instagram.com/explore/tags/creatyumlayered/"
+                      rel="noreferrer noopener"
+                      target="_blank"
+                      className="underline"
+                    >
+                      #creatyumlayered
+                    </Link>
+                  </b>
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div className="w-full">
-            {loading && <p>Cargando imagen...</p>}
-            {error && <p>{error}</p>}
-            {comicImageUrl && (
+            <div className="w-full">
               <Image
-                src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${comicImageUrl}`}
+                src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${imageUrl}`}
                 alt="creatyum-layered-comic"
                 width={512}
                 height={512}
                 className="w-full h-auto object-cover rounded-2xl border border-yellow dark:border-black md:rounded-3xl"
               />
-            )}
+            </div>
           </div>
         </div>
+      </SidebarContainer>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <div>
+        <h1>Error fetching data</h1>
+        <p>{error.message}</p>
       </div>
-    </SidebarContainer>
-  );
-};
-
-export default SidebarComic;
+    );
+  }
+}
--- src/components/sidebar/SidebarLatestArticles.jsx
+++ src/components/sidebar/SidebarLatestArticles.jsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,25 +6,9 @@ import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 import { getLatestArticles } from "@lib/api";
 import SidebarContainer from "@ui/sidebar/SidebarContainer";
 
-const SidebarLatestArticles = () => {
-  const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    fetchLatestArticles();
-  }, []);
-
-  const fetchLatestArticles = async () => {
-    setIsLoading(true);
-    try {
-      const data = await getLatestArticles(5);
-      setArticles(data);
-    } catch (error) {
-      console.error("Error fetching latest articles:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+export default async function SidebarLatestArticles() {
+  const data = await getLatestArticles(5);
+  const articles = data;
 
   return (
     <SidebarContainer>
@@ -34,47 +16,41 @@ const SidebarLatestArticles = () => {
         <h2 className="font-extrabold text-lg mb-8 uppercase">
           Últimos Artículos
         </h2>
-        {isLoading ? (
-          <p className="text-center">Cargando artículos...</p>
-        ) : (
-          <ul className="list-none p-0">
-            {articles.map((article) => (
-              <Link href={`/articulo/${article.slug}`} key={article.id}>
-                <li className="flex items-center space-x-4 mb-8">
-                  {article.cover && article.cover && (
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${article.cover.url}`}
-                      alt={article.title}
-                      width={480}
-                      height={270}
-                      className="w-16 h-16 object-cover rounded-full border border-yellow md:w-20 md:h-20 dark:border-black"
+
+        <ul className="list-none p-0">
+          {articles.map((article) => (
+            <Link href={`/articulo/${article.slug}`} key={article.id}>
+              <li className="flex items-center space-x-4 mb-8">
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${article.cover.url}`}
+                  alt={article.title}
+                  width={480}
+                  height={270}
+                  className="w-16 h-16 object-cover rounded-full border border-yellow md:w-20 md:h-20 dark:border-black"
+                />
+
+                <div className="flex flex-col justify-center">
+                  <h3 className="text-2xl font-extrabold hover:underline">
+                    {article.title}
+                  </h3>
+                  <p className="text-sm mt-2">
+                    <FontAwesomeIcon
+                      icon={faCalendarDays}
+                      className="mr-1 w-3 h-3 align-baseline"
                     />
-                  )}
-                  <div className="flex flex-col justify-center">
-                    <h3 className="text-2xl font-extrabold hover:underline">
-                      {article.title}
-                    </h3>
-                    <p className="text-sm mt-2">
-                      <FontAwesomeIcon
-                        icon={faCalendarDays}
-                        className="mr-1 w-3 h-3 align-baseline"
-                      />
-                      {new Intl.DateTimeFormat("es-ES", {
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                        timeZone: "America/El_Salvador",
-                      }).format(new Date(article.date))}
-                    </p>
-                  </div>
-                </li>
-              </Link>
-            ))}
-          </ul>
-        )}
+                    {new Intl.DateTimeFormat("es-ES", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                      timeZone: "America/El_Salvador",
+                    }).format(new Date(article.date))}
+                  </p>
+                </div>
+              </li>
+            </Link>
+          ))}
+        </ul>
       </div>
     </SidebarContainer>
   );
-};
-
-export default SidebarLatestArticles;
+}
