4382d6dec08dd106b00a11e57d90b8cde439b6e0
Alejandro Martir <hola@alemartir.com>
2024-10-22 20:22:14 -0600

    Refactorize fetch content

--- src/app/(main)/terminos/page.jsx
+++ src/app/(main)/terminos/page.jsx
@@ -1,44 +1,98 @@
-import TermsPageContent from "./TermsPageContent";
+import React from "react";
+import { getTermsContent } from "@lib/api";
+import FormatContent from "@ui/FormatContent";
 
-export const metadata = {
-  title: "Términos de uso | Creatyum Media",
-  description:
-    "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
-  openGraph: {
-    title: "Términos de uso | Creatyum Media",
-    description:
-      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
-    url: "https://creatyum.media/terminos",
-    type: "website",
-    images: [
-      {
-        url: "https://creatyum.media/creatyum-default-cover.webp",
-        width: 1200,
-        height: 630,
-        alt: "Creatyum Media",
+export async function generateMetadata() {
+  try {
+    const termsData = await getTermsContent();
+    const { title } = termsData;
+
+    if (!title) {
+      return undefined;
+    }
+
+    return {
+      title: `${title} | Creatyum Media`,
+      description:
+        "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+      openGraph: {
+        title: `${title} | Creatyum Media`,
+        description:
+          "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+        url: "https://creatyum.media/terminos",
+        type: "website",
+        images: [
+          {
+            url: "https://creatyum.media/creatyum-default-cover.webp",
+            width: 1200,
+            height: 630,
+            alt: "Creatyum Media",
+          },
+        ],
       },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Términos de uso | Creatyum Media",
-    description:
-      "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
-    images: ["https://creatyum.media/creatyum-default-cover.webp"],
-  },
-  canonical: "https://creatyum.media/terminos",
-};
+      twitter: {
+        card: "summary_large_image",
+        title: `${title} | Creatyum Media`,
+        description:
+          "En Creatyum ofrecemos artículos y podcasts sobre diseño y creatividad que educan, empoderan y amplían tu perspectiva en el sector creativo.",
+        images: ["https://creatyum.media/creatyum-default-cover.webp"],
+      },
+      canonical: "https://creatyum.media/terminos",
+    };
+  } catch (error) {
+    console.error("Error fetching about content:", error);
 
-export default function TermsPage() {
-  return (
-    <div className="container mx-auto px-4 py-2 md:px-0">
-      <h1>Términos de uso</h1>
+    return undefined;
+  }
+}
 
-      <div className="grid grid-cols-12 gap-4 md:gap-12">
-        <div className="col-span-12 md:col-span-8">
-          <TermsPageContent />
+export default async function TermsPage() {
+  try {
+    const termsData = await getTermsContent();
+
+    const { title, date, content } = termsData;
+    const formattedDate = date
+      ? new Intl.DateTimeFormat("es-ES", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }).format(new Date(date))
+      : "Fecha no disponible";
+
+    return (
+      <div className="container mx-auto px-4 py-2 md:px-0">
+        <h1>{title}</h1>
+
+        <div className="grid grid-cols-12 gap-4 md:gap-12">
+          {/* MAIN CONTENT */}
+          <div className="col-span-12 md:col-span-8">
+            <h2 className="font-extrabold text-5xl pb-16 md:text-7xl">
+              {title}
+            </h2>
+
+            {/* UPDATE BADGE */}
+            <div
+              href="/patrocinado"
+              target="_blank"
+              className="mb-8 bg-yellow text-black py-1 px-5 text-xs rounded-full border border-black inline-block dark:bg-black dark:text-yellow dark:border-yellow"
+            >
+              <p className="font-bold uppercase">
+                Actualizado: {formattedDate}
+              </p>
+            </div>
+
+            <FormatContent blocks={content} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <div>
+        <h1>Error fetching data</h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
 }
